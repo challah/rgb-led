77e37f2136245cc405c8fb3d2f20b89e59bcc22b
Glitch (hello-webpage) <none>
2019-09-17 22:20:05 +0000

    🎻🌮 Checkpoint ./index.html:369863/386 ./style.css:369863/183 ./script.js:369863/971

--- script.js
+++ script.js
@@ -2,4 +2,12 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log('hi');
+
+const eyeColour = document.querySelector('.eye');
+console.log(eyeColour);
+
+const red = 0;
+
+function changeEyeColour (){
+  eyeColour.setAttribute = ('background-color', `rgb(${red}, ${green}, ${blue}`)
+}
