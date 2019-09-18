15f22b73d25c66e91559a0566587e9a0673d8cfe
Glitch (rgb-led) <none>
2019-09-18 01:47:02 +0000

    ⏲🌂 Checkpoint ./script3.js:399545/1713 ./index.html:399545/2

--- script3.js
+++ script3.js
@@ -16,43 +16,39 @@ createButtons();
 let activeColour
 let colourDirection
 
+const eyes = {
+  red: 0,
+  green: 0,
+  blue: 0
+}
+
 const menu = document.querySelector('#menu');
 menu.addEventListener('click', function(e) {
-  if (e.target.className.includes('red')) {
-    activeColour = red;
-  }
-  if (e.target.className.includes('green')) {
-    activeColour = green;
-  }
-  if (e.target.className.includes('blue')) {
-    activeColour = blue;
-  }
   if (e.target.className.includes('up')) {
     colourDirection = 'up'
   }
   if (e.target.className.includes('down')) {
     colourDirection = 'down'
   }
-  changeEyeColour(activeColour, colourToChange);
-  console.log(activeColour)
+  if (e.target.className.includes('red')) {
+    activeColour = 'red';
+  }
+  if (e.target.className.includes('green')) {
+    activeColour = 'green';
+  }
+  if (e.target.className.includes('blue')) {
+    activeColour = 'blue';
+  }
+  console.log(eyes);
+  changeEyeColour(eyes, activeColour, colourDirection);
+  eye[0].style.backgroundColor = `rgb(${eyes.red}, ${eyes.green}, ${eyes.blue})`;
+  eye[1].style.backgroundColor = `rgb(${eyes.red}, ${eyes.green}, ${eyes.blue})`;
 });
 
-function changeColour(colourName, colour, direction) {
-  if (direction === 'up') {
-    colour += 5;
-  } else if (direction === 'down') {
-    colour -= 5;
+function changeEyeColour(eyes, colour, direction) {
+  if (direction === 'up' && eyes[colour] <= 255) {
+    eyes[colour] += 5;
+  } else if (direction === 'down' && eyes[colour] >= 0) {
+    eyes[colour] -= 5;
   }
-}
-
-function changeEyeColour (){
-  if (colourDirection === 'up') {
-  activeColour += 5;
-  }
-  if (colourDirection === 'down') {
-  activeColour -= 5;
-  }
-  eye[0].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  eye[1].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  console.log(eye[0])
 }
