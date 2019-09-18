ad8d8bf48dab81aa9e501bc1a511bab51b31ad4e
Glitch (rgb-led) <none>
2019-09-18 00:24:00 +0000

    🚲🐏 Checkpoint ./script.js:369863/1413 ./index.html:369863/9

--- script.js
+++ script.js
@@ -10,17 +10,19 @@ const eye = document.querySelectorAll('.eye');
 
 function changeEyeColour (){
   red += 5;
-  
   eye[0].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
   console.log(eye[0]);
 }
 
 const colours = ['red', 'green', 'blue']
-const menu = document.querySelector('#menu');
-// menu.innerHTML = buttons();
-function buttons () {
-  for (let i = 0; i > colours.length-1; i++) {
-     var node = `${colours[i]}: <button class="${colours[i]} up">↑</button><button class="${colours[i]} down">↓</button>`
-  document.getElementById("menu").appendChild(node)
+function createButtons() {
+    for (let i = 0; i < colours.length; i++) {
+        const div = document.createElement('div');
+        div.setAttribute('id', colours[i]);
+        document.querySelector('#menu').appendChild(div);
+        div.innerHTML = `${colours[i]}: <button class="${colours[i]} up">↑</button><button class="${colours[i]} down">↓</button>`
+    }
 }
-}
+createButtons();
+
+const menu = document.
