f2bf65015aac622215b09a3f86c87cd8029654c0
Glitch (rgb-led) <none>
2019-09-18 00:36:00 +0000

    💁🎟 Checkpoint ./script.js:369863/1191

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-
-// prints "hi" in the browser's dev tools console
-
 const eye = document.querySelectorAll('.eye');
   let red = 0;
   let green = 0;
   let blue = 0;
 
-function changeEyeColour (){
-  red += 5;
-  eye[0].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  console.log(eye[0]);
-}
 
 const colours = ['red', 'green', 'blue']
 function createButtons() {
@@ -24,5 +14,26 @@ function createButtons() {
     }
 }
 createButtons();
+let activeColour
+let colourDirection
 
-const menu = document.
+const menu = document.querySelector('#menu');
+menu.addEventListener('click', function(e) {
+  if (e.target.className.includes('red')) {
+    activeColour = 'red'
+  }
+  if (e.target.className.includes('up')) {
+    colourDirection = 'up'
+  }
+});
+
+function changeEyeColour (){
+  if (colourDirection === 'up') {
+  activeColour += 5;
+  }
+  if (colourDirection === 'down') {
+    activ
+  }
+  eye[0].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+  console.log(eye[0]);
+}
